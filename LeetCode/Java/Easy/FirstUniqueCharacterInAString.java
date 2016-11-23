09fb5c5f9ae9f6f77869e7d562730cd88412d978
Jashan Sudan <jashansudan@gmail.com>
2016-11-22 22:41:43 -0500

    better solution to a problem

--- LeetCode/Java/Easy/FirstUniqueCharacterInAString.java
+++ LeetCode/Java/Easy/FirstUniqueCharacterInAString.java
@@ -5,21 +5,20 @@ public class FirstUniqueCharacterInAString {
 
 	public static void main(String[] args) {
 		
-		System.out.println(firstUniqueChar("car"));
+		System.out.println(firstUniqueChar("lleettcoode"));
 	}
 	
 	public static int firstUniqueChar(String s){
 		int[] count = new int[26];
 		for (int i=0; i<s.length(); i++){
-			char temp = s.charAt(i);
-			count[Character.getNumericValue(temp)- Character.getNumericValue('a')]++;
+			count[s.charAt(i)- 'a']++;
 		}
 		for (int j=0; j < s.length(); j++){
-			char temp = s.charAt(j);
-			if (count[Character.getNumericValue(temp)- Character.getNumericValue('a')] == 1){
+			if (count[s.charAt(j) - 'a'] == 1){
 				return j;
 			}
 		}
+		
 		
 		
 		return 0;
