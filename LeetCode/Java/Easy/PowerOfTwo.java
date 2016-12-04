6a5cf719e6cfbc0d2da56967315ee6ccb51146d2
Jashan Sudan <jashansudan@gmail.com>
2016-12-03 22:10:09 -0500

    done power of two

---
+++ LeetCode/Java/Easy/PowerOfTwo.java
@@ -0,0 +1,19 @@
+package Easy;
+
+public class PowerOfTwo {
+	
+	public static void main(String[] args){
+		
+	}
+	
+	public boolean isPowerOfTwo(int n){
+		if (n==0){
+			return false;
+		}
+		if (n == 1){
+			return true;
+		}
+		
+		return n%2==0 && isPowerOfTwo(n/2);
+	}
+}
