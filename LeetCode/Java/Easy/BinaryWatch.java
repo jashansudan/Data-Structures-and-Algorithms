27cf6dccbfde88322a9280502764f41d4cc071c2
Jashan Sudan <jashansudan@gmail.com>
2016-12-20 21:32:27 -0500

    finished binary watch, with some help

---
+++ LeetCode/Java/Easy/BinaryWatch.java
@@ -0,0 +1,23 @@
+package Easy;
+
+import java.util.*;
+
+public class BinaryWatch {
+
+	public static void main(String[] args) {
+
+	}
+	
+	public static List<String> readBinaryWatch(int num){
+		List<String> times = new ArrayList<String>();
+		for (int h =0; h < 12; h++){
+			for (int m=0; m < 60; m++){
+				if (Integer.bitCount(h) + Integer.bitCount(m) == num){
+					times.add(String.format("%d:%02d", h, m));
+				}
+			}
+		}
+		return times;
+	}
+
+}
