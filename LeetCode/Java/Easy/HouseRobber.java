5535745102994592c2c7785b3a1aec443d3feb5f
Jashan Sudan <jashansudan@gmail.com>
2017-01-04 18:18:07 -0500

    finished house robber, with help

---
+++ LeetCode/Java/Easy/HouseRobber.java
@@ -0,0 +1,23 @@
+package Easy;
+
+public class HouseRobber {
+
+	public static void main(String[] args) {
+
+	}
+	
+	public static int rob (int[] nums){
+		int a = 0;
+		int b = 0;
+		
+		for (int i = 0; i < nums.length; i++){
+			if (i%2 == 0){
+				a = Math.max(a + nums[i], b);
+			} else {
+				b = Math.max(a, b +nums[i]);
+			}
+		}
+		return Math.max(a, b);
+	}
+
+}
