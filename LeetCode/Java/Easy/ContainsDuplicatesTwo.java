7127a7b979b4bb67c3fcfe3665484476173585c8
Jashan Sudan <jashansudan@gmail.com>
2017-01-23 22:45:48 -0800

    finished contains duplicates two

---
+++ LeetCode/Java/Easy/ContainsDuplicatesTwo.java
@@ -0,0 +1,28 @@
+package Easy;
+
+import java.util.*;
+
+public class ContainsDuplicatesTwo {
+
+	public void main(String[] args) {
+
+	}
+	
+	public boolean containsNearbyDuplicate(int[] nums, int k){
+		Set<Integer> set = new HashSet<>();
+		
+		for (int i=0;i < nums.length; i++){
+			if (i > k){
+				set.remove(nums[i-k-1]);
+			}
+			if (!set.add(nums[i])){
+				return true;
+			}
+		}
+		
+		
+		
+		return false;
+	}
+
+}
