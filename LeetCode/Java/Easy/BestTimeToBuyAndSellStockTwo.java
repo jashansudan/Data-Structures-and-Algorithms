80944e1e0cff7fd54d0d2bbf44daa8afdc667600
Jashan Sudan <jashansudan@gmail.com>
2017-02-21 23:38:02 -0800

    finished best time to buy and sell stock 2, might look for different solution later

---
+++ LeetCode/Java/Easy/BestTimeToBuyAndSellStockTwo.java
@@ -0,0 +1,19 @@
+package Easy;
+
+public class BestTimeToBuyAndSellStockTwo {
+
+	public static void main(String[] args) {
+
+	}
+	
+	public int maxProfit(int[] prices){
+		int profit = 0;
+		for (int i = 0; i <prices.length-1; i++){
+			if (prices[i + 1] > prices[i]){
+				profit += prices[i + 1] - prices[i];
+			}
+		}
+		return profit;
+	}
+
+}
