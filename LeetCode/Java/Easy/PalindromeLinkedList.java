b74a77ebdd4dd00f90571bb6bbf6073d080847fe
Jashan Sudan <jashansudan@gmail.com>
2017-07-04 22:57:38 -0400

    used help to make constant time solution

--- LeetCode/Java/Easy/PalindromeLinkedList.java
+++ LeetCode/Java/Easy/PalindromeLinkedList.java
@@ -9,28 +9,39 @@ public class PalindromeLinkedList {
 	}
 	
 	public boolean isPalindrome(ListNode head){
-		int len = 0;
-		ListNode ptr = head;
-		while (ptr != null){
-			ptr = ptr.next;
-			len++;
+		ListNode fast = head;
+		ListNode slow = head;
+		while (fast != null && fast.next != null){
+			fast = fast.next.next;
+			slow = slow.next;
 		}
-		ptr = head;
 		
-		Stack<Integer> stk = new Stack();
-		int count = 0;
-		while(len/2 > count){
-			stk.push(ptr.val);
-			ptr = ptr.next;
-			count++;
+		if (fast != null){
+			slow = slow.next;
 		}
-		if (len %2 == 1) ptr = ptr.next;
 		
-		while (ptr != null){
-			if (stk.pop() != ptr.val) return false;
-			ptr = ptr.next;
+		slow = reverse(slow);
+		fast = head;
+		
+		while (slow != null){
+			if (fast.val != slow.val){
+				return false;
+			}
+			fast = fast.next;
+			slow = slow.next;
 		}
 		return true;
+	}
+	
+	public ListNode reverse(ListNode head){
+		ListNode prev = null;
+		while (head != null) {
+			ListNode next = head.next;
+			head.next = prev;
+			prev = head;
+			head = next;
+		}
+		return prev;
 	}
 	
 
